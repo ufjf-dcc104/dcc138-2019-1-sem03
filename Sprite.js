00163821ac59ef42f4d0530ec320e19b481d240e
Igor Knop <igorknop@gmail.com>
2019-08-19 20:06:13 -0300

    Adiciona modelo de criação

--- Sprite.js
+++ Sprite.js
@@ -1,24 +1,31 @@
-function Sprite() {
-    this.x = 0;
-    this.y = 0;
+function Sprite(exemplo) {
+    var {
+        x = 10,
+        y = 100,
+        w = 10,
+        vx = 0,
+        vy = 0
+    } = exemplo;
+    this.x = x;
+    this.y = y;
 
-    this.w = 10;
+    this.w = w;
 
-    this.vx = 0;
-    this.vy = 0;
-
+    this.vx = vx;
+    this.vy = vy;
+ 
 }
 
-Sprite.prototype = new Sprite();
+Sprite.prototype = new Sprite({});
 Sprite.constructor = Sprite;
 
 Sprite.prototype.desenhar = function (ctx) {
-    ctx.fillStyle="blue";
-    ctx.strokeStyle="black";
-    ctx.fillRect(this.x, this.y,this.w, this.w);
+    ctx.fillStyle = "blue";
+    ctx.strokeStyle = "black";
+    ctx.fillRect(this.x, this.y, this.w, this.w);
 }
 
-Sprite.prototype.mover = function(dt){
-    this.x = this.x + this.vx*dt;
-    this.y = this.y + this.vy*dt;
+Sprite.prototype.mover = function (dt) {
+    this.x = this.x + this.vx * dt;
+    this.y = this.y + this.vy * dt;
 }
