5fec5bf6a96a2b1a79bdbcc9c094fb5821b9c534
Igor Knop <igorknop@gmail.com>
2019-08-19 19:47:28 -0300

    Adiciona protótipo a Sprite

--- Sprite.js
+++ Sprite.js
@@ -2,9 +2,18 @@ function Sprite() {
     this.x = 0;
     this.y = 0;
 
-    this.w = 30;
+    this.w = 10;
 
     this.vx = 0;
     this.vy = 0;
 
+}
+
+Sprite.prototype = new Sprite();
+Sprite.constructor = Sprite;
+
+Sprite.prototype.desenhar = function (ctx) {
+    ctx.fillStyle="blue";
+    ctx.strokeStyle="black";
+    ctx.fillRect(this.x, this.y,this.w, this.w);
 }
